d46e5b5bf09f2d8d81642cd2321a3c515b0aa71e
Freyja <78993276+Frey1a@users.noreply.github.com>
2022-03-12 21:26:56 +0700

    upfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
+echo "Setting up your Pc"
 
-# Install Xcode
-xcode-select --install
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which yay); then
+   pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 fi
 
-# Install all our dependencies with bundle (See Brewfile)
-echo "Installing brewfile"
-chmod +x brew.sh
-./brew.sh
+
 
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
@@ -23,19 +18,18 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 cp $HOME/.dotfiles/themes/nt9.zsh-theme $HOME/.oh-my-zsh/themes/
 
-# Install VimPlug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install VimPlug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Install Haskell Stack
-echo "Installiing Stack..."
-curl -sSL https://get.haskellstack.org/ | sh
+# Install 
+echo "Installiing ..."
 
-echo "Installing Rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Tmux plugins Manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Installing ..."
+
+
+
 
 echo "Setting up symlink..."
 
@@ -43,35 +37,17 @@ echo "Setting up symlink..."
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
 
-# Symlink gitconfig
-rm -rf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
 
-# Symlink vimrc
-rm -rf $HOME/.vimrc
-ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 
 # Symlink neovim
 rm -rf $HOME/.config/nvim/init.vim
 mkdir -p .config/nvim
-ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim/
 
-# Symlink ideavimrc
-rm -rf $HOME/.ideavimrc
-ln -s $HOME/.dotfiles/ideavimrc $HOME/.ideavimrc
 
-# Symlink tmux
-rm -rf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
-# Install fzf key bindings and fuzzy compeletion:
-$(brew --prefix)/opt/fzf/install
 
-# Install py3 for Neovim
-pip3 install pynvim
 
-# Install Ansible
-pip3 install ansible
 
 # https://stackoverflow.com/questions/43433542/stuck-at-android-repositories-cfg-could-not-be-loaded
 touch ~/.android/repositories.cfg
---
+++ pacman.sh
@@ -0,0 +1,16 @@
+#Pacman 
+sudo pacman -S git 
+sudo pacman -S zsh
+sudo pacman -S alacritty
+
+#for neovim
+sudo pacman -S neovim
+sudo pacman -S ccls 
+sudo pacman -S nodejs
+sudo pacman -S yarn
+sudo pacman -S npm 
+sudo pacman -S python3-dev
+
+
+#yay 
+yay -S polybar
