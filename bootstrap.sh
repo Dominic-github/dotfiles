4750386df611378c87d965e26ec88f9018f1e477
Freyja <78993276+Frey1a@users.noreply.github.com>
2022-03-21 11:27:51 +0700

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+
 
 echo "Setting up your Pc"
 
--- install.sh
+++ install.sh
@@ -1,12 +1,2 @@
-#!/bin/sh
 
-echo "Downloading dotfiles"
-curl -LJO 
-echo "Unziping dotfiles and start..."
-unzip -a dotfiles-master.zip
-mv -f dotfiles-master .dotfiles
-rm dotfiles-master.zip
-
-cd $HOME/.dotfiles
-chmod +x pacman.sh
-./pacman.sh
+bash ~/.dotfiles/pacman.sh
